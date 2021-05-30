6c69bb802979b78abe9524d021889d8dfd51f271
ZetaZeta <pavi690@gmail.com>
2021-05-30 21:00:42 +0200

    SaerchFragment

--- app/src/main/java/triocalavera/freenomo/SearchForCategoryViewModel.kt
+++ app/src/main/java/triocalavera/freenomo/SearchForCategoryViewModel.kt
@@ -1,8 +1,81 @@
 package triocalavera.freenomo
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.AndroidViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import triocalavera.freenomo.Adapter.PostAdapter
+import triocalavera.freenomo.Model.Post
+import triocalavera.freenomo.databinding.FragmentHomeBinding
+import triocalavera.freenomo.databinding.SearchForCategoryFragmentBinding
 
 class SearchForCategoryViewModel(application: Application) : AndroidViewModel(application) {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+    private lateinit var _binding: SearchForCategoryFragmentBinding
+    private var post = mutableListOf<Post>()
 
+    @SuppressLint("StaticFieldLeak")
+    private lateinit var _activity: FragmentActivity
+    fun init(
+        bindig: SearchForCategoryFragmentBinding,
+        requireActivity: FragmentActivity
+    ) {
+        auth = FirebaseAuth.getInstance()
+        database = Firebase.database.reference
+        _binding = bindig
+        _activity = requireActivity
+
+    }
+
+    fun obtenerPost() {
+        database.child("post").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    post.clear()
+                    for (c: DataSnapshot in dataSnapshot.children) {
+                        post.add(
+                            Post(
+                                c.child("uuid").value.toString(),
+                                c.child("titulo").value.toString(),
+                                c.child("categoria").value.toString(),
+                                c.child("foto").value.toString(),
+                                c.child("descripcion").value.toString(),
+                                c.child("telefono").value.toString(),
+                                c.child("precio").value.toString()
+                            )
+                        )
+                    }
+                    getPost()
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.d("INFO", "loadPost:onCancelled", databaseError.toException())
+            }
+        })
+    }
+
+    private fun getPost() {
+        val grid = _binding.grid
+
+        val categoryPost = mutableListOf<Post>()
+        post.forEach {
+            if (it.categoria.equals("Programación",false)){
+                categoryPost.add(it)
+            }
+        }
+        val categoryAdapter = PostAdapter(_binding.root.context, categoryPost)
+        _binding.progresBarHome.visibility = View.INVISIBLE
+        grid.adapter = categoryAdapter
+    }
 }
--- app/src/main/java/triocalavera/freenomo/searchForCategory.kt
+++ app/src/main/java/triocalavera/freenomo/searchForCategory.kt
@@ -4,19 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import triocalavera.freenomo.databinding.SearchForCategoryFragmentBinding
 
 class searchForCategory : Fragment() {
+    private lateinit var searchForCategoryViewModel: SearchForCategoryViewModel
     private lateinit var database: DatabaseReference
     private lateinit var viewModel: SearchForCategoryViewModel
-    val args: searchForCategoryArgs by navArgs()
     private lateinit var binding: SearchForCategoryFragmentBinding
+
+    val args: searchForCategoryArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,8 +30,11 @@ class searchForCategory : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Toast.makeText(context, "${args.categoria} ", Toast.LENGTH_SHORT).show()
-        database = FirebaseDatabase.getInstance().reference
+
+        viewModel = ViewModelProvider(this).get(SearchForCategoryViewModel::class.java)
+        viewModel.init(binding,requireActivity())
+        viewModel.obtenerPost()
+
     }
 
 }
