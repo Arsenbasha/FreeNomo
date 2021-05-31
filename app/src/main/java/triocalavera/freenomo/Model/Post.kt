e09e10cd5f4dbdf35d76fa11fbb685babc8f2409
paredes.llerena.leandro.joseph <URCqNeZR>
2021-05-31 21:59:10 +0200

    call ok -- fix bug

--- app/src/main/java/triocalavera/freenomo/Model/Post.kt
+++ app/src/main/java/triocalavera/freenomo/Model/Post.kt
@@ -1,14 +1,16 @@
 package triocalavera.freenomo.Model
 
+import android.annotation.SuppressLint
 import android.os.Parcel
 import android.os.Parcelable
 
 
-data class Post(val uuid:String, val titulo:String,val categoria:String,val foto:String,val descripcion:String, val numero:String,val precio:String  ):
+@SuppressLint("ParcelCreator")
+data class Post(val uuid:String, val titulo:String, val categoria:String, val foto:String, val descripcion:String, val numero:String, val precio:String  ):
     Parcelable
    {
        override fun writeToParcel(dest: Parcel?, flags: Int) {
-           TODO("Not yet implemented")
+
        }
 
        override fun describeContents(): Int {
